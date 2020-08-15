b5c2a3cbc11b509f3bba02860f17ac40c132feaf
avperillo <avperillo@hotmail.com>
2020-08-15 14:34:32 +0200

    working on argsparser

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import logging as log
 import time
+import argparse
 from datetime import datetime
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from playsound import playsound
 
 MINUTES: int= 1
+SOUND_FILE: str= './echo.mpeg'
 
-def run():
+def run(minutes: int, sound_file: str):
     log.info("Running SoundScheduler")
 
     now = datetime.now()
@@ -18,7 +20,7 @@ def run():
         , 'interval'
         , id = "sound_scheduler"
         , start_date= start_date
-        , minutes= MINUTES)
+        , minutes= minutes)
     scheduler.start()
 
 def configure_log():
@@ -33,11 +35,19 @@ def configure_log():
 
 def play_sound():
     log.info('playing sound')
-    playsound('./sound.mp3')
+    playsound(SOUND_FILE)
 
 if __name__ == "__main__":
     configure_log()
-    run()
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-m', '--minutes', type=int, metavar='minutes', default= 30 
+                            help='time elapsed in minutes')
+    parser.add_argument('-s', '--file', type=str, metavar='sound_file', default= './echo.mpeg' 
+                            help='sound file')
+    args = my_parser.parse_args()
+
+    run(args.minutes, args.sound_file)
 
     # for to keep the app running
     while True:
